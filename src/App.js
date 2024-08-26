7898397c389652ca7a37784f64136c9499632595
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-08-26 00:04:50 -0500

    website prototype with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="leftHeader">
+          <p>home</p>
+        </div>
+        <div className="rightHeader">
+          <p>about</p>
+        </div>
+      </div>
+      <div className="main">
+        <h1>Creator's Toolkit</h1>
+        <p>Everything you could ever need</p>
+        <div className="toolbox">
+          <div className="tool">
+            <h3>Video Converter</h3>
+          </div>
+          <div className="tool">
+            <h3>Screen Recorder</h3>
+          </div>
+          <div className="tool">
+            <h3>Music Downloader</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
