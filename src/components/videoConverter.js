2a9085c80e151df2756db59839e2163590c8f05c
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-09-02 21:37:24 -0500

    begin ffmpeg coding

--- src/components/videoConverter.js
+++ src/components/videoConverter.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import React, { useState, useRef } from 'react';
+import { toBlobURL } from '@ffmpeg/util';
 
 export const VideoConverter = () => {
 
     const [fileName, setFileName] = useState(null);
     const [errorMsg, setErrorMsg] = useState('');
+    // const [fileN, setFile] = useState(null);
 
 
     //get the file from drag and drop
@@ -35,6 +38,7 @@ export const VideoConverter = () => {
         else{
             setErrorMsg('Only webM supported');
         }
+
     }
 
 
@@ -42,6 +46,41 @@ export const VideoConverter = () => {
     const handleDragOver = (e)=>{
         e.preventDefault();
     }
+
+
+
+    //-----------ffmpeg stuff-----------
+    const[loaded, setLoaded] = useState(false);
+    const ffmpegRef = useRef(new FFmpeg());
+    const videoRef = useRef(null);
+    const messageRef = useRef(null);
+
+    const load = async () =>{
+        const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd';
+        const ffmpeg = ffmpegRef.current;
+        ffmpeg.on('log', ({message}) => {
+            messageRef.current.innerHTML = message;
+            console.log(message);
+        })
+        await ffmpeg.load({
+            coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+            wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+        });
+        setLoaded(true);
+    }
+
+    const transcode = async (file, inFileType, outFileType)=>{
+        const ffmpeg = ffmpegRef.current;
+        const inFileName = 'input'+ inFileType;
+        const outFileName = 'output'+ outFileType;
+        await ffmpeg.writeFile(inFileName, file );
+        await ffmpeg.exec(['-i',inFileName, outFileName]);
+        const data = await ffmpeg.readFile(outFileName);
+        videoRef.current.src = URL.createObjectURL(new Blob([data.buffer],{type:'video/mp4'}));
+    }
+
+
+
 
 
 
